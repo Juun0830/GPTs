8fba70e8ef2a896eed3654486337bb468cf8b259
ZZI.nyang <120887138+Juun0830@users.noreply.github.com>
2024-04-04 23:52:22 +0900

    Update Challenge_5.py

--- Challenge/Challenge_5.py
+++ Challenge/Challenge_5.py
@@ -16,7 +16,7 @@ from langchain.storage import LocalFileStore
 from langchain.memory import ConversationBufferMemory
 
 # Chat
-from openai import OpenAI
+import openai
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import RunnablePassthrough,RunnableLambda
@@ -89,11 +89,8 @@ st.title("⭐️RAG Challenge⭐️")
 api_key = st.sidebar.text_input("Enter your OpenAI API Key:", type="password")
 
 if api_key:
-	
-    import os 
-    client = OpenAI(
-		api_key=os.environ.get(f"{api_key}"),
-		)
+ 
+    openai.api_key=f'{api_key}'
 
     llm = ChatOpenAI(
 		temperature=0.1,
